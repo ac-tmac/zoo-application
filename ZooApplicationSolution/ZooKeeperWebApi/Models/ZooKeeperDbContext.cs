ece872f5bbde90fddcaa31917a9e7a906759828b
ac-tmac <tonyisaway@hotmail.com>
2016-11-23 18:44:36 +0000

    Adding a new Animal
    Javascript Call to GET an existing animal added.
    Javascript Call to POST a new animal added.
    WebAPI GET and POST added for animal.
    Added EF, and DbContext.

---
+++ ZooApplicationSolution/ZooKeeperWebApi/Models/ZooKeeperDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ZooKeeperWebApi.Models
+{
+    public class ZooKeeperDbContext : DbContext
+    {
+        public ZooKeeperDbContext() : base("name=DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
