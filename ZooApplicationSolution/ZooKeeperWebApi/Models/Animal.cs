5032a180d82dffa7bd90bd5a7e9f8f3af61d347a
ac-tmac <tonyisaway@hotmail.com>
2016-11-24 20:06:30 +0000

    Changed API to use a DTO of animal instead of the model animal

--- ZooApplicationSolution/ZooKeeperWebApi/Models/Animal.cs
+++ ZooApplicationSolution/ZooKeeperWebApi/Models/Animal.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
 namespace ZooKeeperWebApi.Models
 {
-    public class Animal
+    public class Animal : AnimalBase
     {
-        [Key]
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public DateTime DateOfBirth { get; set; }
     }
 }
