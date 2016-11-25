fc42a0d0aadaa4cb610cc018f1056bc3429eda92
ac-tmac <tonyisaway@hotmail.com>
2016-11-25 08:15:01 +0000

    Added respository

---
+++ ZooApplicationSolution/ZooKeeperWebApi/Interfaces/AnimalRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ZooKeeperWebApi.Models;
+
+namespace ZooKeeperWebApi.Interfaces
+{
+    public class AnimalRepository
+    {
+        private ZooKeeperDbContext zooKeeperDb;
+        public AnimalRepository(ZooKeeperDbContext zooKeeperDb)
+        {
+            this.zooKeeperDb = zooKeeperDb;
+        }
+
+        public Animal Get(Guid id)
+        {
+            var animal = zooKeeperDb.Animals.SingleOrDefault(x => x.Id == id);
+            return animal;
+        }
+    }
+}
