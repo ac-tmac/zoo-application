977112f5f0d1f6b40f4e64a7775a4752ad6dca65
ac-tmac <tonyisaway@hotmail.com>
2016-11-27 13:06:16 +0000

    Removed commented code

--- ZooApplicationSolution/ZooKeeperWebApi/Controllers/AnimalsController.cs
+++ ZooApplicationSolution/ZooKeeperWebApi/Controllers/AnimalsController.cs
@@ -12,21 +12,7 @@ namespace ZooKeeperWebApi.Controllers
 
         public IEnumerable<IAnimal> Get()
         {
-            //TODO: How to make this work?
             return zooKeeperDb.Animals.OrderBy(x => x.Name);
-
-
-            //var parrots = zooKeeperDb.Parrots as IEnumerable<AnimalBase>;
-            //var ostrichs = zooKeeperDb.Ostrich as IEnumerable<AnimalBase>;
-            //var tigers = zooKeeperDb.Tiger as IEnumerable<AnimalBase>;
-            //var elephants = zooKeeperDb.Elephant as IEnumerable<AnimalBase>;
-
-            //var animals = new List<AnimalBase>();
-            //animals.AddRange(parrots);
-            //animals.AddRange(ostrichs);
-            //animals.AddRange(tigers);
-            //animals.AddRange(elephants);
-            //return animals;
         }
     }
 }
